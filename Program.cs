71f1a8d141b971d0901b2eed736c37c63e7fd05f
KarinaSmu <Smumrrr@mail.ru>
2023-02-15 22:33:23 +0300

    add ProgrammFile

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintAray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите ограничение по символам:");
+int word = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длинну строкового массива:");
+int limit = int.Parse(Console.ReadLine());
+
+string[] original = new string[limit];
+int k = 0;
+string[] result = new string[limit];
+string str_user = string.Empty;
+int count = 1;
+for (int i = 0; i < limit; i++)
+{
+    Console.WriteLine("Введите " + count + " сроку:");
+    str_user = Console.ReadLine();
+    original[i] = str_user;
+    count++;
+    if (original[i].Length < word)
+    {
+        result[k] = str_user;
+        k++;
+    }
+}
+
+Console.WriteLine("Ваш исходный массив:");
+PrintAray(original);
+Console.WriteLine();
+Console.WriteLine("Ваш итоговый массив:");
+PrintAray(result);
+
